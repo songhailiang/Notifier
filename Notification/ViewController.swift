b5e26d7948246ede132878d613842686ea29ac00
hailiang.song <hailiang.song@nuelion.com.cn>
2018-03-28 10:32:46 +0800

    Initial Commit

---
+++ Notification/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Notification
+//
+//  Created by songhailiang on 28/03/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
